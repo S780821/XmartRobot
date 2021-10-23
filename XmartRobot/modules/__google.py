1923a9ecfc60e2da28afa7ed3f6046df8bffca3a
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-10-23 13:08:45 +0530

    Update __google.py

--- XmartRobot/modules/__google.py
+++ XmartRobot/modules/__google.py
@@ -21,9 +21,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from XMARTROBOT import *
+from XmartRobot import *
 
-from XMARTROBOT.events import register
+from XmartRobot.events import register
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
